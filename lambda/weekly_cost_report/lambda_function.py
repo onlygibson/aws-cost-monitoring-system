18d8eabe265046c200c96697d9e2c4ca43aa10df
onlygibson <ibehgibson@yahoo.com>
2026-01-18 18:38:46 -0800

    Add AWS cost report generation to Lambda function

--- lambda/weekly_cost_report/lambda_function.py
+++ lambda/weekly_cost_report/lambda_function.py
@@ -1,1 +1,36 @@
+import boto3
+import json
+from datetime import date, timedelta
+
+ce = boto3.client('ce')
+s3 = boto3.client('s3')
+
+BUCKET_NAME = 'aws-cost-reports-gibson'
+
+def lambda_handler(event, context):
+    end = date.today()
+    start = end - timedelta(days=7)
+
+    response = ce.get_cost_and_usage(
+        TimePeriod={
+            'Start': start.strftime('%Y-%m-%d'),
+            'End': end.strftime('%Y-%m-%d')
+        },
+        Granularity='DAILY',
+        Metrics=['UnblendedCost']
+    )
+
+    file_name = f"weekly-report-{end}.json"
+
+    s3.put_object(
+        Bucket=BUCKET_NAME,
+        Key=file_name,
+        Body=json.dumps(response, indent=2),
+        ContentType='application/json'
+    )
+
+    return {
+        'statusCode': 200,
+        'body': f'Report saved to S3 as {file_name}'
+    }
 
